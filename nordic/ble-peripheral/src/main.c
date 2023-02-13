2726c38ed40dbdb4b192d0e60418ac56320f35f7
Mohammad Afaneh <mafaneh@gmail.com>
2023-02-13 12:01:18 -0500

    Added new LED Service which supports read and write (turning on/off the LED #3 on the DK and allowing the client to read its current status)

--- nordic/ble-peripheral/src/main.c
+++ nordic/ble-peripheral/src/main.c
@@ -23,7 +23,7 @@
 #include <dk_buttons_and_leds.h>
 
 // Device Name
-#define DEVICE_NAME     "BLE-PERIPHERAL"
+#define DEVICE_NAME     "BLE-LED"
 #define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
 
 // LEDs
@@ -31,12 +31,84 @@
 #define CON_STATUS_LED          DK_LED2
 #define RUN_LED_BLINK_INTERVAL  1000
 
+// User LED
+#define USER_LED                DK_LED3
+
+// Store the current value of the User LED
+static uint8_t led_value = 0;
+
+/** @brief LBS Service UUID. */
+#define BT_UUID_SIMPLE_SERVICE_VAL \
+	BT_UUID_128_ENCODE(0x65210001, 0x28D5, 0x4B7B, 0xBADF, 0x7DEE1E8D1B6D)
+
+// Service:        Simple Service UUID      65210001-28D5-4B7B-BADF-7DEE1E8D1B6D
+static struct bt_uuid_128 simple_service_uuid = 
+    BT_UUID_INIT_128(0x6d, 0x1b, 0x8d, 0x1e, 0xee, 0x7d, 0xdf, 0xba, 0x7b,
+             0x4b, 0xd5, 0x28, 0x01, 0x00, 0x21, 0x65);
+
+// Characteristic: LED1 Characteristic UUID 65210002-28D5-4B7B-BADF-7DEE1E8D1B6D
+static struct bt_uuid_128 led1_char_uuid = 
+    BT_UUID_INIT_128(0x6d, 0x1b, 0x8d, 0x1e, 0xee, 0x7d, 0xdf, 0xba, 0x7b,
+             0x4b, 0xd5, 0x28, 0x02, 0x00, 0x21, 0x65);
+
 // Advertising Data
 static const struct bt_data ad[] = {
-	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
 	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+	BT_DATA_BYTES(BT_DATA_UUID128_ALL, BT_UUID_SIMPLE_SERVICE_VAL)
 };
 
+// LED1 read value function
+static ssize_t read_led(struct bt_conn *conn,
+			       const struct bt_gatt_attr *attr, void *buf,
+			       uint16_t len, uint16_t offset)
+{
+
+    printk("Read value of User LED (DK LED 3): 0x%02x\n", led_value);
+
+    return bt_gatt_attr_read(conn, attr, buf, len, offset, &led_value, sizeof(led_value));
+}
+
+void led_change_state(uint8_t new_state)
+{
+    printk("Received value 0x%02x. Turning LED %s\n", new_state, (new_state? "ON":"OFF"));
+
+	dk_set_led(DK_LED3, new_state);
+}
+
+// LED1 write value function
+static ssize_t write_led(struct bt_conn *conn,
+               const struct bt_gatt_attr *attr, const void *buf,
+               uint16_t len, uint16_t offset, uint8_t flags)
+{
+    printk("Received value for LED1: 0x%02x\n", ((uint8_t *) buf)[0]);
+
+    uint8_t value = ((uint8_t *) buf)[0];
+
+    // Change the state of LED1
+    led_change_state(value);
+	led_value = value;
+
+    return len;
+}
+
+// Instantiate the service and its characteristics
+BT_GATT_SERVICE_DEFINE(
+    simple_service,
+    
+    // Simple Service
+    BT_GATT_PRIMARY_SERVICE(&simple_service_uuid),
+
+    // LED1 Characteristic
+    // Properties: Read, Write
+    BT_GATT_CHARACTERISTIC(&led1_char_uuid.uuid,
+                    BT_GATT_CHRC_READ | BT_GATT_CHRC_WRITE,
+                    BT_GATT_PERM_READ | BT_GATT_PERM_WRITE,
+                    read_led,
+                    write_led,
+                    &led_value)
+);
+
+// Connected state callback
 static void connected(struct bt_conn *conn, uint8_t err)
 {
 	if (err) {
@@ -49,6 +121,7 @@ static void connected(struct bt_conn *conn, uint8_t err)
 	dk_set_led_on(CON_STATUS_LED);
 }
 
+// Disconnected state callback
 static void disconnected(struct bt_conn *conn, uint8_t reason)
 {
 	printk("Disconnected (reason %u)\n", reason);
@@ -56,6 +129,7 @@ static void disconnected(struct bt_conn *conn, uint8_t reason)
 	dk_set_led_off(CON_STATUS_LED);
 }
 
+// Register BLE connection callbacks
 BT_CONN_CB_DEFINE(conn_callbacks) = {
 	.connected        = connected,
 	.disconnected     = disconnected,
